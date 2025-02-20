cc86c05136c7af80c90c2fe528cc477bee140b0a
Sravan34 <68266636+Sravan34@users.noreply.github.com>
2025-02-20 14:54:52 +0000

    test commit

--- ContactManagementApp1/ContactManagementApp1/Controllers/ContactsController.cs
+++ ContactManagementApp1/ContactManagementApp1/Controllers/ContactsController.cs
@@ -66,7 +66,7 @@ namespace ContactManagementApp1.Controllers
             System.IO.File.WriteAllText(@"Contacts.json",newJson);
             return contact1;
         }
-
+     // this is empty line
     }
 
 }
